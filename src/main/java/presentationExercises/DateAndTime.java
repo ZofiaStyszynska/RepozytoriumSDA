96d5f245ecc019474342953e820798e0d199c83c
Zofia Styszynska <zofia.styszynska@gmail.com>
2020-06-20 13:31:28 +0200

    Zadania data i czas

---
+++ src/main/java/presentationExercises/DateAndTime.java
@@ -0,0 +1,37 @@
+package presentationExercises;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTime {
+
+    public static void currentLocalTime() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Aktualny czas to: " + localDateTime);
+        Instant now = Instant.now();
+        System.out.println(now);
+    }
+
+    public static void tokyoDateTime() {
+        // Strefy dokumentacja https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/
+        Clock clock = Clock.system(ZoneId.of("Asia/Tokyo"));
+        LocalDateTime ldt = LocalDateTime.now(clock);
+        System.out.println("Japan time:" + ldt);
+    }
+
+    public static void dateFormat() {
+        LocalDateTime ldt = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss");
+        System.out.println("Date in format yyyy/MM/dd yyyy/MM/dd hh:mm:ss: " + formatter.format(ldt));
+    }
+
+
+    public static void main(String[] args) {
+        currentLocalTime();
+        tokyoDateTime();
+        dateFormat();
+    }
+}
---
+++ src/main/java/presentationExercises/ForEach.java
@@ -0,0 +1,29 @@
+package presentationExercises;
+
+public class ForEach {
+
+    public static void ex1(String[] tableOfNames){
+        for (String name:tableOfNames) {
+            System.out.println("Hello " + name);
+        }
+    }
+
+    public static String ex2(String[] arrayOfProducts){
+        String list = "";
+        for (String product:arrayOfProducts) {
+            list += product+ ", ";
+
+        }
+        return list;
+    }
+
+    public static void main(String[] args){
+       String[] names = {"Agata", "Julia", "Miłosz"};
+       String[] products = {"szynka", "masło", "pomidor"};
+       ex1(names);
+
+       String result = ex2(products);
+       System.out.println(result);
+
+    }
+}
