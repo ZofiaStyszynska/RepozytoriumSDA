09ac3a31122c14c88b83508a2fb5eac86904abd1
Zofia Styszynska <zofia.styszynska@gmail.com>
2020-06-21 09:58:09 +0200

    Przerobiony kod Hmw2

--- src/main/java/presentationExercises20june/HomeworkSDA2.java
+++ src/main/java/presentationExercises20june/HomeworkSDA2.java
@@ -3,27 +3,36 @@ package presentationExercises20june;
 import java.util.Scanner;
 
 public class HomeworkSDA2 {
+
+
+    public static boolean checkPrimeNum(int a) {
+        boolean isDivisible = false;
+
+        for (int i = 2; i < a/2; i++) {
+            int result = a % i;
+            if (result == 0) {
+                isDivisible = true;
+                System.out.println("Znaleziono dzielnik: "+ i);
+                break;
+            }
+        }
+       return isDivisible;
+
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj dowolną liczbę całkowitą większą od 1");
 
         int a = scanner.nextInt();
-        boolean isDivisible = false;
-        int b=0;
 
-        for (int i = 2; i < a; i++) {
-            int result = a%i;
-            if (result==0){
-                isDivisible=true;
-                b=i;
-                break;
-            }
-        }
-        if (isDivisible){
-            System.out.println("To nie jest liczba pierwsza. Ta liczba jest podzielna przez " + b);
-        }else {
+        boolean isPrime = checkPrimeNum(a);
+        if (isPrime) {
+            System.out.println("To nie jest liczba pierwsza.");
+        } else {
             System.out.println("To jest liczba pierwsza.");
         }
+
     }
 }
