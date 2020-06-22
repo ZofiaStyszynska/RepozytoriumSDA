eda99d6c23bc17584fa6f9927ba2b2e7bf5e62a1
Zofia Styszynska <zofia.styszynska@gmail.com>
2020-06-22 22:45:50 +0200

    Hmw1mz_ex3 operacje na ArrayList Double

---
+++ src/main/java/Homework21june/Kolekcje1mz_ex3.java
@@ -0,0 +1,49 @@
+package Homework21june;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kolekcje1mz_ex3 {
+    public static void addFirstLast(List<Double> list) {
+        double firstNumber = list.get(0);
+        double lastNumber = list.get(list.size() - 1);
+        double sum = firstNumber + lastNumber;
+        System.out.println(firstNumber + " + " + lastNumber + " = " + sum);
+    }
+
+    public static void multiplyFirstLast(List<Double> list) {
+        double firstNumber = list.get(0);
+        double lastNumber = list.get(list.size() - 1);
+        double product = firstNumber * lastNumber;
+        System.out.println(firstNumber + " * " + lastNumber + " = " + product);
+
+
+    }
+    public static void divideSecPenult(List<Double> list) {
+        double secondNumber = list.get(1);
+        double penultNumber = list.get(list.size() - 2);
+        double product = secondNumber / penultNumber;
+        System.out.println(secondNumber + " / " + penultNumber + " = " + product);
+
+
+    }
+
+    public static void main(String[] args) {
+        List<Double> floatPointNums = new ArrayList<>();
+        floatPointNums.add(23.7);
+        floatPointNums.add(47.98);
+        floatPointNums.add(87.9);
+        floatPointNums.add(347.45);
+        floatPointNums.add(76.09);
+        floatPointNums.add(3.908);
+        floatPointNums.add(4.9);
+        floatPointNums.add(98.7);
+        floatPointNums.add(78.9);
+        floatPointNums.add(8.067);
+
+        addFirstLast(floatPointNums);
+        multiplyFirstLast(floatPointNums);
+        divideSecPenult(floatPointNums);
+        //System.out.println(floatPointNums.get(0)+floatPointNums.get(floatPointNums.size()-1));
+    }
+}
